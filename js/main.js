50b2d8aa72158753917c5e01b3297e68c23cd98f
Gabriel 111Skin <gabriel@111skin.com>
2018-04-10 17:39:06 +0100

    added first form and basic style

--- js/main.js
+++ js/main.js
@@ -2,16 +2,29 @@ const quiz = document.querySelector('.quiz')
 
 const headerButton = document.querySelector('.header-button');
 
+const q1 = document.querySelector('.q1');
+
+
 let customerData = [];
 
 function init() {
- quiz.style.display = 'block';
-};
-
-headerButton.addEventListener('click', function(){
 	if (quiz.style.display == 'block') {
 		quiz.style.display = 'none';
+		q1.style.display = 'none';
 	} else 
-		quiz.style.display = 'block';	
-});
+		quiz.style.display = 'block'
+		 q1.style.display = 'block';
+};
+
+// const answersOption = [...document.querySelectorAll('.answer-option')];
+
+// answersOption.forEach(function(x){
+// 	x.addEventListener('click', function(){
+// 		 // x.classList.toggle('active');
+// 	});
+// })
+
+
+headerButton.addEventListener('click', init);
+
 
