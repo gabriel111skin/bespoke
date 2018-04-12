cbe738d088e05a1b76d76c8a04510fada67393d6
Gabriel 111Skin <gabriel@111skin.com>
2018-04-12 18:13:12 +0100

    Added 3rd option and final result page removing the buttons;refresh to restart ATM

--- js/main.js
+++ js/main.js
@@ -4,15 +4,16 @@ const headerButton = document.querySelector('.header-button');
 
 const q1 = document.querySelector('.q1');
 const q2 = document.querySelector('.q2');
+const q3 = document.querySelector('.q3');
 
 const button = [...document.querySelectorAll('.button')];
-
+const buttons = document.querySelector('.navigation-buttons');
 const savePackageLocal = JSON.parse(localStorage.getItem('savePackageLocal')) || [];
 
 let answers = {
 	numberOfProducts : {},
 	skinType : {},
-	age : {},
+	goal : {},
 }
 
 let customerData = [];
@@ -20,8 +21,6 @@ let customerData = [];
 function init() {
 	if (quiz.style.display == 'block') {
 		quiz.style.display = 'none';
-		q1.style.display = 'none';
-		q2.style.display = 'none';
 	} else 
 		quiz.style.display = 'block'
 		 q1.style.display = 'block';
@@ -33,19 +32,31 @@ button[1].addEventListener('click', function() {
 	if (q1.style.display == 'block') {
 		answers.numberOfProducts = document.querySelector('input[name=number_of_products]:checked').value;
 		q1.style.display = 'none';
-		q2.style.display = 'block';
-		
-	}
-	else if (q2.style.display = 'block') {
+		q2.style.display = 'block';		
+	} 
+	if (q2.style.display = 'block') {
 		answers.skinType = document.querySelector('input[name=skin_products]:checked').value;
-		
+		q2.style.display = 'none';
+		q3.style.display = 'block'
+	} 
+	if (q3.style.display = 'block') {
+		answers.goal = document.querySelector('input[name=goal_products]:checked').value;
+		q3.style.display = 'none';
+		buttons.style.display = 'none';
+		quiz.append(`
+		You choose: ${answers.numberOfProducts.toUpperCase()} products |
+		Skin type: ${answers.skinType.toUpperCase()} |
+		Looking to achieve a ${answers.goal.toUpperCase()} skin`);
+
 	}
+
+	
 	savePackageLocal.splice(0, savePackageLocal.length);
 
 	savePackageLocal.push(answers);
 
 	localStorage.setItem('savePackageLocal', JSON.stringify(savePackageLocal));
-		headerButton.innerHTML = `You chose ${answers.numberOfProducts} products with a skin type of ${answers.skinType}`;
+		
 })
 
 headerButton.addEventListener('click', init);
